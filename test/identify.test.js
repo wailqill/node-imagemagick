6e549b1760e8c21c3715f4c55d07ec5f549c4bcd
Thiago Arrais <thiago.arrais@gmail.com>
2011-06-08 19:27:17 -0300

    Tests for identify input via string and buffer

--- test/identify.test.js
+++ test/identify.test.js
@@ -2,7 +2,15 @@ var assert = require('assert'),
     imagemagick = require('../lib/imagemagick'),
     EventEmitter = require('events').EventEmitter;
 
-var TestStream = function() {
+var TestWriter = function () {
+  this.writes = [];
+  this.ends = [];
+  this.setEncoding = function() {};
+  this.write = function() { this.writes.push(arguments); };
+  this.end = function() { this.ends.push(arguments); };
+};
+
+var TestReader = function() {
   this.emitter = new EventEmitter();
   this.addListener = this.emitter.addListener.bind(this.emitter);
   this.setEncoding = function() {};
@@ -12,13 +20,25 @@ var TestChild = function() {
   this.emitter = new EventEmitter();
   this.kill = function() {};
   this.addListener = this.emitter.addListener.bind(this.emitter);
-  this.stdout = new TestStream();
-  this.stderr = new TestStream();
+  this.stdout = new TestReader();
+  this.stderr = new TestReader();
+  this.stdin = new TestWriter();
+};
+TestChild.prototype.run = function(data) {
+  if (data === undefined) {
+    data =
+      [ 'Image: /tmp/picture.jpg',
+        '  Format: JPEG (Joint Photographic Experts Group JFIF format)',
+        '  Geometry: 1024x768+0+0',
+        '  Depth: 8-bit'].join("\n")
+  }
+  this.stdout.emitter.emit('data', data);
+  this.emitter.emit('exit', 0, null);
 };
 
 var TestProc = function() {
-this.spawns = [];
-this.child = new TestChild();
+  this.spawns = [];
+  this.child = new TestChild();
 
   this.spawn = function(prog, args) {
     this.spawns.push([prog, args]);
@@ -33,12 +53,11 @@ module.exports = {
         features;
 
     im.identify('/tmp/fake.jpg', function(err, f) { features = f; });
-    proc.child.stdout.emitter.emit('data',
+    proc.child.run(
       [ "Image: /tmp/fake.jpg",
         "  Format: JPEG (Joint Photographic Experts Group JFIF format)",
         "  Geometry: 1278x626+0+0",
         "  Depth: 8-bit"].join("\n"));
-    proc.child.emitter.emit('exit', 0, null);
 
     assert.equal('identify', proc.spawns[0][0]);
     assert.equal('-verbose', proc.spawns[0][1][0]);
@@ -47,5 +66,26 @@ module.exports = {
     assert.equal(1278, features.width);
     assert.equal(626, features.height);
     assert.equal(8, features.depth);
+  },
+  'test feeds string data into child proc': function() {
+    var proc = new TestProc(),
+        im = imagemagick.config(proc);
+
+    im.identify({data: "testing"}, function() {});
+    proc.child.run();
+
+    assert.equal('testing', proc.child.stdin.writes[0][0]);
+    assert.equal('binary', proc.child.stdin.writes[0][1]);
+  },
+  'test feeds buffer data into child proc': function() {
+    var proc = new TestProc(),
+        im = imagemagick.config(proc),
+        buf = new Buffer(32);
+
+    im.identify({data: buf}, function() {});
+    proc.child.run();
+
+    assert.equal(buf, proc.child.stdin.ends[0][0],
+      'Buffer passed in expected to be passed as is to the imagemagick child process');
   }
 };
