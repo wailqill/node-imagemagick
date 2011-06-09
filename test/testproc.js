05988b4a2e88a98a458d981ffde387af960a8ae7
Thiago Arrais <thiago.arrais@gmail.com>
2011-06-09 19:22:09 -0300

    Extract TestProc and make the test a little more concise

---
+++ test/testproc.js
@@ -0,0 +1,78 @@
+var assert = require('assert'),
+    EventEmitter = require('events').EventEmitter;
+
+var toArray = function(args, n) { return Array.prototype.slice.call(args, n); };
+var arrayEqual = function(actual, expected, msg) {
+  assert.equal(actual.length, expected.length, msg);
+  for(i in actual) {
+    if (actual[i] instanceof Array) arrayEqual(actual[i], expected[i], msg);
+    else assert.equal(actual[i], expected[i], msg);
+  }
+};
+
+var call = function() {
+  var callargs = toArray(arguments);
+
+  return {with: function() {
+    var msg, args = toArray(arguments);
+    if (args[0] instanceof Array) {
+      args = args[0];
+      msg = args[1];
+    }
+    arrayEqual(args, callargs, msg);
+  }};
+};
+
+var funcstub = function(func) {
+  var result = function() {
+    var args = toArray(arguments);
+    result.calls.push(call.apply(undefined, args));
+    if (func !== undefined) return func.apply(undefined, args);
+  };
+  result.calls = [];
+  return result;
+};
+
+var stub = function(obj) {
+  var funcnames = toArray(arguments, 1);
+  funcnames.map(function(name) {obj[name] = funcstub();});
+};
+
+var TestWriter = function () {
+  stub(this, 'setEncoding', 'write', 'end');
+};
+
+var TestReader = function() {
+  this.emitter = new EventEmitter();
+  this.addListener = this.emitter.addListener.bind(this.emitter);
+  stub(this, 'setEncoding');
+};
+
+var TestChild = function() {
+  this.emitter = new EventEmitter();
+  this.addListener = this.emitter.addListener.bind(this.emitter);
+  this.stdout = new TestReader();
+  this.stderr = new TestReader();
+  this.stdin = new TestWriter();
+  stub(this, 'kill');
+};
+TestChild.prototype.run = function(data) {
+  if (data === undefined) {
+    data =
+      [ 'Image: /tmp/picture.jpg',
+        '  Format: JPEG (Joint Photographic Experts Group JFIF format)',
+        '  Geometry: 1024x768+0+0',
+        '  Depth: 8-bit'].join("\n")
+  }
+  this.stdout.emitter.emit('data', data);
+  this.emitter.emit('exit', 0, null);
+};
+
+var TestProc = function() {
+  this.spawns = [];
+  var child = this.child = new TestChild();
+
+  this.spawn = funcstub(function() { return child; });
+};
+module.exports = TestProc;
+
