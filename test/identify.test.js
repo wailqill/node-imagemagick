98112db1e4f4c50b03507032fb660791e9300c18
Thiago Arrais <thiago.arrais@gmail.com>
2011-06-09 19:16:32 -0300

    Less ugly stubbing

--- test/identify.test.js
+++ test/identify.test.js
@@ -2,27 +2,60 @@ var assert = require('assert'),
     imagemagick = require('../lib/imagemagick'),
     EventEmitter = require('events').EventEmitter;
 
+var toArray = function(args, n) { return Array.prototype.slice.call(args, n); };
+var arrayEqual = function(actual, expected, msg) {
+  assert.equal(actual.length, expected.length, msg);
+  for(i in actual) {
+    if (actual[i] instanceof Array) arrayEqual(actual[i], expected[i], msg);
+    else assert.equal(actual[i], expected[i], msg);
+  }
+};
+
+var call = function() {
+  var callargs = toArray(arguments);
+
+  return {with: function() {
+    var msg, args = toArray(arguments);
+    if (args[0] instanceof Array) {
+      args = args[0];
+      msg = args[1];
+    }
+    arrayEqual(args, callargs, msg);
+  }};
+};
+
+var funcstub = function(func) {
+  var result = function() {
+    var args = toArray(arguments);
+    result.calls.push(call.apply(undefined, args));
+    if (func !== undefined) return func.apply(undefined, args);
+  };
+  result.calls = [];
+  return result;
+};
+
+var stub = function(obj) {
+  var funcnames = toArray(arguments, 1);
+  funcnames.map(function(name) {obj[name] = funcstub();});
+};
+
 var TestWriter = function () {
-  this.writes = [];
-  this.ends = [];
-  this.setEncoding = function() {};
-  this.write = function() { this.writes.push(arguments); };
-  this.end = function() { this.ends.push(arguments); };
+  stub(this, 'setEncoding', 'write', 'end');
 };
 
 var TestReader = function() {
   this.emitter = new EventEmitter();
   this.addListener = this.emitter.addListener.bind(this.emitter);
-  this.setEncoding = function() {};
+  stub(this, 'setEncoding');
 };
 
 var TestChild = function() {
   this.emitter = new EventEmitter();
-  this.kill = function() {};
   this.addListener = this.emitter.addListener.bind(this.emitter);
   this.stdout = new TestReader();
   this.stderr = new TestReader();
   this.stdin = new TestWriter();
+  stub(this, 'kill');
 };
 TestChild.prototype.run = function(data) {
   if (data === undefined) {
@@ -38,12 +71,9 @@ TestChild.prototype.run = function(data) {
 
 var TestProc = function() {
   this.spawns = [];
-  this.child = new TestChild();
+  var child = this.child = new TestChild();
 
-  this.spawn = function(prog, args) {
-    this.spawns.push([prog, args]);
-    return this.child;
-  };
+  this.spawn = funcstub(function() { return child; });
 };
 
 module.exports = {
@@ -59,9 +89,7 @@ module.exports = {
         "  Geometry: 1278x626+0+0",
         "  Depth: 8-bit"].join("\n"));
 
-    assert.equal('identify', proc.spawns[0][0]);
-    assert.equal('-verbose', proc.spawns[0][1][0]);
-    assert.equal('/tmp/fake.jpg', proc.spawns[0][1][1]);
+    proc.spawn.calls[0].with('identify', ['-verbose', '/tmp/fake.jpg'])
     assert.equal('JPEG', features.format);
     assert.equal(1278, features.width);
     assert.equal(626, features.height);
@@ -74,8 +102,7 @@ module.exports = {
     im.identify({data: "testing"}, function() {});
     proc.child.run();
 
-    assert.equal('testing', proc.child.stdin.writes[0][0]);
-    assert.equal('binary', proc.child.stdin.writes[0][1]);
+    proc.child.stdin.write.calls[0].with('testing', 'binary');
   },
   'test feeds buffer data into child proc': function() {
     var proc = new TestProc(),
@@ -85,7 +112,7 @@ module.exports = {
     im.identify({data: buf}, function() {});
     proc.child.run();
 
-    assert.equal(buf, proc.child.stdin.ends[0][0],
+    proc.child.stdin.end.calls[0].with([buf],
       'Buffer passed in expected to be passed as is to the imagemagick child process');
   }
 };
